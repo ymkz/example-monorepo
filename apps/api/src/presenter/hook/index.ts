229786503c1afe03d164aca9a4ce1c5f6eb72107
ymkz <ymkz@users.noreply.github.com>
2024-04-13 22:31:05 +0900

    chore: improve validation error resposne

--- apps/api/src/presenter/hook/index.ts
+++ apps/api/src/presenter/hook/index.ts
@@ -1,6 +1,6 @@
 import type { Context, ErrorHandler, NotFoundHandler } from 'hono'
 import type { ZodError } from 'zod'
-// import { fromZodError } from 'zod-validation-error'
+import { fromZodError } from 'zod-validation-error'
 import { logger } from '~/utils/log'
 
 export const notFoundHandler: NotFoundHandler = (ctx) => {
@@ -11,7 +11,7 @@ export const notFoundHandler: NotFoundHandler = (ctx) => {
   return ctx.json(
     {
       title: 'Not Found',
-      type: 'E_NOTFOUND',
+      type: '',
       detail: 'requested path not found',
       status: 404,
     },
@@ -24,8 +24,8 @@ export const errorHandler: ErrorHandler = (err, ctx) => {
   return ctx.json(
     {
       title: 'Internal Server Error',
-      type: 'E_INTERNALSERVERERROR',
-      detail: 'an unexpected error has occurred',
+      type: '',
+      detail: 'unexpected error has occurred',
       status: 500,
     },
     500,
@@ -36,14 +36,16 @@ type Result = { success: true; data: unknown } | { success: false; error: ZodErr
 
 export const validationHook = (result: Result, ctx: Context) => {
   if (!result.success) {
-    logger.warn(result.error.flatten(), 'バリデーションでエラーが発生しました')
+    logger.warn(
+      { validationError: fromZodError(result.error).details },
+      'バリデーションでエラーが発生しました',
+    )
     return ctx.json(
       {
         title: 'Bad Request',
-        type: 'E_BADREQUEST',
-        detail: result.error.message,
+        type: '',
+        detail: fromZodError(result.error).message,
         status: 500,
-        // reason: fromZodError(result.error).message,
       },
       400,
     )
